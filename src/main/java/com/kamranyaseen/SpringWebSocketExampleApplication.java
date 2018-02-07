eff57479d9ac536e3cb42c4db3f47cd4c0567606
Kamran Yaseen <kamranmyaseen@gmail.com>
2018-02-07 13:41:03 +0300

    Spring WebSocket Application with SpringBoot + SockJS + STOMP
    Spring WebSocket Application with SpringBoot + SockJS + STOMP

---
+++ src/main/java/com/kamranyaseen/SpringWebSocketExampleApplication.java
@@ -0,0 +1,12 @@
+package com.kamranyaseen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSocketExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSocketExampleApplication.class, args);
+	}
+}
